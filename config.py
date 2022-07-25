6eee6508df8daab89e3e7a74b6af62d57615d454
Antonio Zizzari <72402942+Antonio-Zizzari@users.noreply.github.com>
2022-07-25 02:41:04 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+
+class DefaultConfig:
+    PORT = 
+    COOKIE_KEY= ''
+    ENDPOINT_URL = ''
+    EMAIL_ISS = ''
+    EMAIL_CANDLE_PRODUCER = ''
+    ID_PRODUCER = 
+    TOKEN_TELEGRAM_BOT = ''
+    MAIL_SWITCH = 'ON'
+    TELEGRAM_SWITCH = 'ON'
---
+++ globalData.py
@@ -0,0 +1,14 @@
+
+class DefaultData:
+    ROOMS = ['room1', 'room2', 'room3']
+    IOT_DEVICES = ['thermometer','humidity_sensor','gas_recognizer','oxygen_shutoff']
+    GAS ={
+        "nitrogen": "78.0",
+        "oxygen": "20.0",
+        "argon": "1.5",
+        "carbon_dioxide": "0.04"
+    }#gas,% avarage in the air
+    QUEUE1= ['gas','humidity','temperature']
+    NUM_IOT_DEVICES= 12
+    ASTRONAUTS= 8
+
